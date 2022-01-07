38e2884dd93337e7e8741de1625a425aabc40d61
mingyin3 <mingyin2001@yahoo.com>
2022-01-07 12:53:34 -0700

    add hello message

--- Application/src/main/java/com/myapp/root/RootApplication.java
+++ Application/src/main/java/com/myapp/root/RootApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RootApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello");
 		SpringApplication.run(RootApplication.class, args);
 	}
 
